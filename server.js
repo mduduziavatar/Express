3b065609907e89b250625de79487de8984cb32ae
mduduziavatar <59861854+mduduziavatar@users.noreply.github.com>
2021-05-19 11:42:41 +0200

    added more changes

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-const express = require('express');
+let express = require('express');
 let path = require('path');
+let cookieSession = require('cookie-session')
+
+let FeedbackService = require('./services/FeedbackService');
+let SpeakerService = require('./services/SpeakerService');
+
+let feedbackService = new FeedbackService('./data/feedback.json');
+let speakerService = new SpeakerService('./data/speakers.json');
 
 let app = express();
 
-let routes = require('./routes')
+let routes = require('./routes');
 
+//for reverse proxy to be able to track app
+app.set('trust proxy', 1);
+
+app.use(cookieSession({
+    name: 'session',
+    keys: ['Randomly', 'Created']
+}))
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
 
@@ -12,7 +26,10 @@ let port = 3000;
 
 app.use(express.static(path.join(__dirname, './static')))
 
-app.use('/', routes());
+app.use('/', routes({
+    feedbackService,
+    speakerService
+}));
 
 app.listen(port, function () {
     console.log(`Express server listening on port ${port}!`);
